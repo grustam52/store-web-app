202e96feaba69c47b0764de27ef900914b3bfc6f
grustam52 <grustam52@gmail.com>
2023-08-05 09:54:43 +0300

    Initial commit

---
+++ src/main/java/ruskypro/storewebapp/model/Cart.java
@@ -0,0 +1,11 @@
+package ruskypro.storewebapp.model;
+
+import java.util.List;
+
+public class Cart {
+    private final List<Integer> items;
+
+    public Cart(List<Integer> items) {
+        this.items = items;
+    }
+}
